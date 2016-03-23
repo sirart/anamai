fa21e2fe22c9556132f05ac0e63615cee4b0b91a
PACNB-030 <PACNB-030@PACNB-030-HP>
2016-03-23 22:45:40 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,8 +1,21 @@
 <?php
-
+use yii\helpers\Html;
 /* @var $this yii\web\View */
-
     $this->title = 'กรมอนามัย';
 ?>
+<?php
+$route1 = Yii::$app->urlManager->createUrl('test/test1');
+?>
+<a href="<?=$route1?>">ไปที่ test1</a>
+<br>
 
-ทดสอบ ttttttesttttt
+<?php
+$route2 = Yii::$app->urlManager->createUrl(['test/test2','name'=>'panuwat','lname'=>'chaladaur']);
+?>
+<a href="<?=$route2?>">ไปที่ test2</a>
+<br>
+
+<?=
+Html::a('ลิงค์แบบที่ 3',['test/test1','a'=>'1']);
+?>
+
